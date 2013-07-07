6204f4265ad89ba60f3b1c46b7510a2e9602182a
Benjamin Landers <benjaminrlanders@gmail.com>
2013-07-06 21:58:17 -0700

    Made a proof of concept

--- app.js
+++ app.js
@@ -23,4 +23,8 @@ io.sockets.on('connection', function (socket) {
   socket.on('my other event', function (data) {
     console.log(data);
   });
+  socket.on('button', function (data) {
+    console.log(data);
+    socket.emit('return', {text:'yoloswagbutton'});
+  });
 });
